b2c7614825912f99499b6f0c98efbb58d080297a
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-02-05 19:21:12 -0300

    Update: Usando Componentes

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
+@NgModule({
+  declarations: [		
+    AppComponent,
+      EventosComponent,
+      PalestrantesComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
