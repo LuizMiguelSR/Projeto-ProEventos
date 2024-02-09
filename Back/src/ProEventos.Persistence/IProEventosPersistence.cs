731738b0ff782ffdf4925efcd8f2239201b25e05
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-02-09 12:36:28 -0300

    Update: Interface Persistência

---
+++ Back/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence
+{
+    public interface IProEventosPersistence
+    {
+        // Geral
+        void Add<T>(T entity) where T: class;
+        void Update<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        void DeleteRange<T>(T[] entity) where T: class;
+        Task<bool> SaveChangeAsync();
+
+        // Eventos
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento> GetAEventoByIdAsync(int EventoId, bool includePalestrantes);
+
+        // Palestrantes
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string Nome, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante> GetAPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+    }
+}
