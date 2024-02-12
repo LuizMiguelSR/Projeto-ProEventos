ce2cfce6447fb8f34221e62cba2776db7ce5f63e
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-02-12 11:33:25 -0300

    Update: Contrato Evento Application

--- Back/src/ProEventos.Application/Contratos/IEventosService.cs
+++ Back/src/ProEventos.Application/Contratos/IEventosService.cs
@@ -1,10 +1,13 @@
 using System.Threading.Tasks;
 using ProEventos.Domain;
 
-namespace ProEventos.Persistence.Contratos
+namespace ProEventos.Application.Contratos
 {
-    public interface IEventoPersist
+    public interface IEventoService
     {
+        Task<Evento> AddEventos(Evento model);
+        Task<Evento> UpdateEvento(int eventoId, Evento model);
+        Task<bool> DeleteEvento(int eventoId);
         // Eventos
         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
