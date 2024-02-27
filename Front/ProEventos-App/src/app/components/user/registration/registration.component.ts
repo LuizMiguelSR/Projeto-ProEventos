19c4cfab582869c920b9b7c97dd57ae7a8bef647
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-02-27 20:18:48 -0300

    Update: Form Registration #01

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validadtion();
+  }
+
+  private validadtion(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['',Validators.required],
+      ultimoNome: ['',Validators.required],
+      email: ['',
+        [Validators.required, Validators.email]
+      ],
+      userName: ['', Validators.required],
+      senha: ['',
+        [Validators.required, Validators.minLength(6)]
+      ],
+      confirmeSenha: ['',
+        [Validators.required, Validators.minLength(6)]
+      ],
+    });
+  }
+
+  public resetForm(): void {
+    this.form.reset();
+  }
 
 }
