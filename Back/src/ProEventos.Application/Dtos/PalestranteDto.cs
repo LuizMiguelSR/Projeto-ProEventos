d06cf23a60b169087aaa375854681847c5e39d6b
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-03-01 18:52:19 -0300

    Update: Classes Dtos Restantes

--- Back/src/ProEventos.Application/Dtos/PalestranteDto.cs
+++ Back/src/ProEventos.Application/Dtos/PalestranteDto.cs
@@ -2,18 +2,14 @@ using System.Collections.Generic;
 
 namespace ProEventos.Application.Dtos
 {
-    public class EventoDto
+    public class PalestranteDto
     {
         public int Id { get; set; }
-        public string Local { get; set; }
-        public string DataEvento { get; set; }
-        public string Tema { get; set; }
-        public int QtdePessoas { get; set; }
-        public string Lote { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurrilo { get; set; }
         public string ImagemURL { get; set; }
-        public int Telefone { get; set; }
+        public string Telefone { get; set; }
         public string Email { get; set; }
-        public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
         public IEnumerable<PalestranteDto> Palestrantes { get; set; }
     }
