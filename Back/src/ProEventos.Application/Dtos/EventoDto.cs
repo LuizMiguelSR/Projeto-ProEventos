dbf2163e7370a0359555dc684e908d8a465e4f3c
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-03-01 19:22:23 -0300

    Update: Data Annotations

--- Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProEventos.Application.Dtos
 {
@@ -7,11 +8,13 @@ namespace ProEventos.Application.Dtos
         public int Id { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Tema { get; set; }
         public int QtdePessoas { get; set; }
         public string Lote { get; set; }
         public string ImagemURL { get; set; }
         public int Telefone { get; set; }
+        [EmailAddress(ErrorMessage = "O campo {0} precisa ser um e-mail válido")]
         public string Email { get; set; }
         public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
