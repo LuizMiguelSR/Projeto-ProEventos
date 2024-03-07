2f9eb3eb7c0c49f3b8ab91e3fcbab0ce8b3dff57
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-03-07 19:07:09 -0300

    Update: Post, Put e Delete

--- Front/ProEventos-App/src/app/services/evento.service.ts
+++ Front/ProEventos-App/src/app/services/evento.service.ts
@@ -19,7 +19,15 @@ export class EventoService {
     return this.http.get<Evento[]>(`${this.baseURL}/${tema}/tema`)
   }
 
-  public getEventoById(id: number): Observable<Evento> {
-    return this.http.get<Evento>(`${this.baseURL}/${id}`)
+  public postEvento(evento: Evento): Observable<Evento> {
+    return this.http.post<Evento>(this.baseURL, evento);
+  }
+
+  public putEvento(evento: number): Observable<Evento> {
+    return this.http.put<Evento>(this.baseURL, evento);
+  }
+
+  public deleteEvento(id: number): Observable<string> {
+    return this.http.delete<string>(`${this.baseURL}/${id}`);
   }
 }
