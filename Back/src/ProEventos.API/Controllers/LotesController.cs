7ad17ec6222d260e155c20672fc9fdc7ea405d14
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-03-11 18:15:22 -0300

    Update: Enpoints Controller Lotes

---
+++ Back/src/ProEventos.API/Controllers/LotesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.Application.Dtos;
+using ProEventos.Application.Contratos;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LotesController : ControllerBase
+    {
+        private readonly ILoteService _loteService;
+        
+        public LotesController(ILoteService loteService)
+        {
+            _loteService = loteService;
+            
+        }
+
+        [HttpGet("{eventoId}")]
+        public async Task<IActionResult> Get(int eventoId)
+        {
+            try
+            {
+                var eventos = await _eventoService.GetEventoByIdAsync(true);
+                if (eventos == null) return NoContent();
+
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{eventoId}")]
+        public async Task<IActionResult> Put(int eventoId, LoteDto[] models)
+        {
+            try
+            {
+                var evento = await _eventoService.UpdateEvento(id, models);
+                if (evento == null) return NoContent();
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar atualizar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{eventoId}/{loteId}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id, true);
+                if (evento == null) return NoContent();
+
+                return await _eventoService.DeleteEvento(id) 
+                        ? Ok( new { message = "Deletado" }) 
+                        : throw new Exception("Ocorreu um problema não específico ao tentar deletar o Evento.");
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar deletar eventos. Erro: {ex.Message}");
+            }
+        }
+    }
+}
