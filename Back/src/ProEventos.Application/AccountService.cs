0617f5a7d12be28c8c49e94cdf763d858fa86637
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2024-03-26 13:15:16 -0300

    Update: Account Service #02

--- Back/src/ProEventos.Application/AccountService.cs
+++ Back/src/ProEventos.Application/AccountService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ProEventos.Application.Contratos;
 using ProEventos.Application.Dtos;
 using ProEventos.Domain.Identity;
@@ -28,29 +29,80 @@ namespace ProEventos.Application
             _mapper = mapper;
             _userPersist = userPersist;
         }
-        public Task<SignInResult> ChecUserPasswordAsync(UserUpdateDto userUpdateDto, string password)
+        public async Task<SignInResult> ChecUserPasswordAsync(UserUpdateDto userUpdateDto, string password)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var user = await _userManager.Users
+                                             .SingleOrDefaultAsync(user => user.UserName == userUpdateDto.UserName.ToLower());
+                return await _signInManager.CheckPasswordSignInAsync(user, password, false);
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception($"Erro ao tentar verificar password. Erro:{ex.Message}");
+            }
         }
 
-        public Task<UnobservedTaskExceptionEventArgs> CreateAccountAsync(UserDto userDto)
+        public async Task<UserDto> CreateAccountAsync(UserDto userDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var user = _mapper.Map<User>(userDto);
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+
+                if (result.Succeeded) 
+                {
+                    var userToReturn = _mapper.Map<UserDto>(user);
+                    return userToReturn;
+                }
+                return null;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception($"Erro ao tentar criar usuário. Erro:{ex.Message}");
+            }
         }
 
-        public Task<UserUpdateDto> GetUserByUsernameAsync(string username)
+        public async Task<UserUpdateDto> GetUserByUserNameAsync(string username)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var user = await _userPersist.GetUserByUserNameAsync(username);
+
+                if (user == null) return null;
+                
+                var userUpdateDto = _mapper.Map<UserUpdateDto>(user);
+                return userUpdateDto;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception($"Erro ao tentar pegar usuário por username. Erro:{ex.Message}");
+            }
         }
 
-        public Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto)
+        public async Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception($"Erro ao tentar atualizar usuário. Erro:{ex.Message}");
+            }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string userName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _userManager.Users
+                                         .AnyAsync(user => user.UserName == userName.ToLower());
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception($"Erro ao verificar se o usuário existe. Erro:{ex.Message}");
+            }
         }
     }
 }
--- Back/src/ProEventos.Application/Contratos/IAccountService.cs
+++ Back/src/ProEventos.Application/Contratos/IAccountService.cs
@@ -10,7 +10,7 @@ namespace ProEventos.Application.Contratos
     public interface IAccountService
     {
         Task<bool> UserExists(string username);
-        Task<UserUpdateDto> GetUserByUsernameAsync(string username);
+        Task<UserUpdateDto> GetUserByUserNameAsync(string username);
         Task<SignInResult> ChecUserPasswordAsync(UserUpdateDto userUpdateDto, string password);
         Task<UnobservedTaskExceptionEventArgs> CreateAccountAsync(UserDto userDto);
         Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto);
